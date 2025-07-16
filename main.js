11b2480f5afd975a6880ecab5d830c8465f14b12
Hakiimdaud <cabdihakiim395@gmail.com>
2025-07-16 13:21:23 -0700

    update min

--- main.js
+++ main.js
@@ -1,29 +1,57 @@
 
 
+// const min = document.querySelector(".min");
+// const plus = document.querySelector(".plus")
+// const one = document.querySelector(".one")
+// const count = document.querySelector(".count")
+
+// let Add =1
+// let Subt =1
+
+// let starvalue =1;
+// const price = 600.75;
+
+
+// plus.addEventListener("click",() =>{
+//     Add ++;
+//     one.innerHTML= Add
+//     starvalue ++
+//     count.innerHTML = "$" + starvalue * price
+
+// })
+// min.addEventListener("click",() =>{
+//     Subt --;
+//     one.innerHTML= Subt
+//     starvalue --
+//     count.innerHTML = "$" + starvalue * price
+  
+// })
+
+
+
+
 const min = document.querySelector(".min");
-const plus = document.querySelector(".plus")
-const one = document.querySelector(".one")
-const count = document.querySelector(".count")
+const plus = document.querySelector(".plus");
+const one = document.querySelector(".one");
+const count = document.querySelector(".count");
 
-let Add =1
-let Subt =1
-
-let starvalue =1;
+let starvalue = 1;
 const price = 600.75;
 
 
-plus.addEventListener("click",() =>{
-    Add ++;
-    one.innerHTML= Add
-    starvalue ++
-    count.innerHTML = "$" + starvalue * price
+one.innerHTML = starvalue;
+count.innerHTML = "$" + (starvalue * price);
 
-})
-min.addEventListener("click",() =>{
-    Subt --;
-    one.innerHTML= Subt
-    starvalue --
-    count.innerHTML = "$" + starvalue * price
-  
-})
+plus.addEventListener("click", () => {
+    starvalue++;
+    one.innerHTML = starvalue;
+    count.innerHTML = "$" + (starvalue * price);
+});
 
+min.addEventListener("click", () => {
+    if (starvalue > 1) { 
+        starvalue--;
+        one.innerHTML = starvalue;
+        count.innerHTML = "$" + (starvalue * price);
+    }
+});
